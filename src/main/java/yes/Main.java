ff54e32a2ad0e4b875397297e06e3be5af5b9f4c
oinoin <beyondthestars628@gmail.com>
2023-02-13 19:47:41 -0500

    ys

--- src/main/java/yes/Main.java
+++ src/main/java/yes/Main.java
@@ -3,38 +3,7 @@ package yes;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.entities.channel.Channel;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import org.jetbrains.annotations.NotNull;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.classroom.ClassroomScopes;
-import com.google.api.services.classroom.model.*;
-import com.google.api.services.classroom.Classroom;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
-import java.util.HashMap;
-
-import javax.security.auth.login.LoginException;
 
 public class Main {
 
@@ -42,7 +11,7 @@ public class Main {
 
 
 
-    public static void main(String[] args) throws GeneralSecurityException, IOException {
+    public static void main(String[] args) {
 
 
         JDA bot = JDABuilder.createDefault("token goes here", GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
@@ -51,13 +20,13 @@ public class Main {
                 .addEventListeners(new Listeners())
                 .build();
 
-        boolean stopwatchallowed = true;
-
         Guild server = bot.getGuildById("1074156189513945179");
 
         if(server != null){
             server.upsertCommand("fart", "farts really hard").queue();
         }
+        //yes
+
 
         //TextChannel general = event.getGuild().getTextChannelById(946147235375251501L);
 
